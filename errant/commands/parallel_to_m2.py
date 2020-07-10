1e00c400851b8bc5ed17eff2c1ba63fc50a780b2
ankur <s.ankursonawane@gmail.com>
2020-07-10 17:28:05 +0530

    Remove unnecessary files

--- errant/commands/parallel_to_m2.py
+++ errant/commands/parallel_to_m2.py
@@ -5,13 +5,15 @@ from annotator import Annotator
 
 def main():
     # Parse command line args
-    print("df")
     args = parse_args()
     print("Loading resources...")
-    # Load Errant
-    annotator = Annotator('hi')
+    # Load Errantsrc/ATen/native/TensorAdvancedIndexing.cpp:573: UserWarning: masked_fill_ received a mask with dtype torch.uint8, this behavior is now deprecated,please use a mask with dtype torch.bool instead.
+
+    annotator = Annotator()
     # Open output m2 file
     out_m2 = open(args.out, "w")
+    error_count = {}
+
     print("Processing parallel files...")
     # Process an arbitrary number of files line by line simultaneously. Python 3.3+
     # See https://tinyurl.com/y4cj4gth
@@ -20,32 +22,32 @@ def main():
         # Process each line of all input files
         for line in zip(*in_files):
             # Get the original and all the corrected texts
-            temp1 = []
-            temp2 = []
             orig = line[0].strip()
-            print(orig)
             cors = line[1:]
-            print(cors)
             # Skip the line if orig is empty
-            if not orig: continue
+            if not orig:
+                continue
             # Parse orig with spacy
             orig = annotator.parse(orig, args.tok)
             # Write orig to the output m2 file
+            temp1 = []
             for sent in orig.sentences:
-              for word in sent.words:
-                temp1.append(word)
-            print("writing data in out.m2")
+                for word in sent.words:
+                    temp1.append(word)
             out_m2.write(" ".join(["S"] + [token.text for token in temp1]) + "\n")
-            for sent in cors.sentences:
-              for word in sent.words:
-                temp2.append(word)
-
+            out_m2.flush()
             # Loop through the corrected texts
             for cor_id, cor in enumerate(cors):
                 cor = cor.strip()
                 # If the texts are the same, write a noop edit
                 if orig.text.strip() == cor:
                     out_m2.write(noop_edit(cor_id) + "\n")
+                    if 'none' in error_count:
+                        error_count['none'] += 1
+                    else:
+                        error_count['none'] = 1
+                    out_m2.flush()
+
                 # Otherwise, do extra processing
                 else:
                     # Parse cor with spacy
@@ -56,17 +58,28 @@ def main():
                     for edit in edits:
                         # Write the edit to the output m2 file
                         out_m2.write(edit.to_m2(cor_id) + "\n")
+                        if edit.type in error_count:
+                            error_count[edit.type] += 1
+                        else:
+                            error_count[edit.type] = 1
+
+                        out_m2.flush()
             # Write a newline when we have processed all corrections for each line
             out_m2.write("\n")
+            out_m2.flush()
+    out_m2.close()
+    error_file = open('error_file', 'w')
+    for key in error_count:
+        error_file.write(key + '\t' + error_count[key])
+    error_file.close()
 
 
 #    pr.disable()
 #    pr.print_stats(sort="time")
-     out_m2.close()
 
 # Parse command line args
 def parse_args():
-    parser=argparse.ArgumentParser(
+    parser = argparse.ArgumentParser(
         description="Align parallel text files and extract and classify the edits.\n",
         formatter_class=argparse.RawTextHelpFormatter,
         usage="%(prog)s [-h] [options] -orig ORIG -cor COR [COR ...] -out OUT")
@@ -81,11 +94,11 @@ def parse_args():
         default=[],
         required=True)
     parser.add_argument(
-        "-out", 
+        "-out",
         help="The output filepath.",
         required=True)
     parser.add_argument(
-        "-tok", 
+        "-tok",
         help="Word tokenise the text using spacy (default: False).",
         action="store_true")
     parser.add_argument(
@@ -95,19 +108,22 @@ def parse_args():
     parser.add_argument(
         "-merge",
         help="Choose a merging strategy for automatic alignment.\n"
-            "rules: Use a rule-based merging strategy (default)\n"
-            "all-split: Merge nothing: MSSDI -> M, S, S, D, I\n"
-            "all-merge: Merge adjacent non-matches: MSSDI -> M, SSDI\n"
-            "all-equal: Merge adjacent same-type non-matches: MSSDI -> M, SS, D, I",
+        "rules: Use a rule-based merging strategy (default)\n"
+        "all-split: Merge nothing: MSSDI -> M, S, S, D, I\n"
+        "all-merge: Merge adjacent non-matches: MSSDI -> M, SSDI\n"
+        "all-equal: Merge adjacent same-type non-matches: MSSDI -> M, SS, D, I",
         choices=["rules", "all-split", "all-merge", "all-equal"],
         default="rules")
-    args=parser.parse_args()
+    args = parser.parse_args()
     return args
 
 # Input: A coder id
 # Output: A noop edit; i.e. text contains no edits
+
+
 def noop_edit(id=0):
-    return "A -1 -1|||noop|||-NONE-|||REQUIRED|||-NONE-|||"+str(id)
-if __name__=="__main__":
-  print("dfd")
-  main()
+    return "A -1 -1|||noop|||-NONE-|||REQUIRED|||-NONE-|||" + str(id)
+
+
+if __name__ == "__main__":
+    main()
